d8e350afaeeed62b3251be92803cd1f2a8fcc1b7
ZakhariaBensoltane <148758703+ZakhariaBensoltane@users.noreply.github.com>
2025-09-16 16:33:38 +0200

    Update dashboard.lua

--- dashboard.lua
+++ dashboard.lua
@@ -1,5 +1,12 @@
-local monitor = peripheral.wrap("left")  -- ou "left"
-monitor.setTextScale(2)
-monitor.clear()
-monitor.setCursorPos(1,1)
-monitor.write("Hello World !")
+local mon = peripheral.wrap("right")
+mon.setTextScale(2)
+mon.setBackgroundColor(colors.black)
+mon.setTextColor(colors.green)
+
+while true do
+    local time = textutils.formatTime(os.time(), true)
+    mon.clear()
+    mon.setCursorPos(3,2)
+    mon.write("⌚ Heure : " .. time)
+    sleep(1)
+end
